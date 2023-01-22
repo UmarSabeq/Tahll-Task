37a5a3aacb2f7637c79fb6dfc7c85762c7b1c04e
UmarSabeq <umar.sabeq75@gmail.com>
2023-01-22 17:24:42 +0200

    Tahll

--- lib/pages/service_provider/widgets/tabs_content.dart
+++ lib/pages/service_provider/widgets/tabs_content.dart
@@ -10,44 +10,39 @@ class TabsContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .13,
-      child: const Flexible(
-          fit: FlexFit.loose,
-          child: TabBarView(children: [
-            Padding(
-              padding: EdgeInsets.only(right: 18, left: 20, bottom: 10),
-              child: Text(
-                'هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى',
-                style: TextStyle(
-                    fontSize: 14,
-                    height: 1.5,
-                    color: textColor,
-                    wordSpacing: 1.5),
-              ),
+      child: TabBarView(children: [
+        Padding(
+          padding: EdgeInsets.only(right: 18, left: 20, bottom: 10),
+          child: Text(
+            'هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى',
+            style: TextStyle(
+                fontSize: 14, height: 1.5, color: textColor, wordSpacing: 1.5),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 18, left: 20, bottom: 10),
+          child: Text(
+            'هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى',
+            style: TextStyle(
+                fontSize: 14,
+                height: 1.5,
+                color: purpleColor,
+                wordSpacing: 1.5),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 18, left: 20, bottom: 10),
+          child: Text(
+            'هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى',
+            style: TextStyle(
+              fontSize: 14,
+              height: 1.5,
+              color: lightBlueColor,
+              wordSpacing: 1.5,
             ),
-            Padding(
-              padding: EdgeInsets.only(right: 18, left: 20, bottom: 10),
-              child: Text(
-                'هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى',
-                style: TextStyle(
-                    fontSize: 14,
-                    height: 1.5,
-                    color: purpleColor,
-                    wordSpacing: 1.5),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(right: 18, left: 20, bottom: 10),
-              child: Text(
-                'هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى',
-                style: TextStyle(
-                  fontSize: 14,
-                  height: 1.5,
-                  color: lightBlueColor,
-                  wordSpacing: 1.5,
-                ),
-              ),
-            ),
-          ])),
+          ),
+        ),
+      ]),
     );
   }
 }
